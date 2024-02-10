a773f0d2004517d645be89155731b7a5ce7bd88c
Skorikov Kirill <skorikovkr@gmail.com>
2024-02-10 08:39:48 +0500

    primevue-tailwind setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,20 @@
+import path from "path";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-})
+  modules: ["@nuxtjs/tailwindcss", "nuxt-primevue"],
+  primevue: {
+    options: {
+      unstyled: true
+    },
+    importPT: { from: path.resolve(__dirname, "./presets/wind-full/").replace(/\\/g, '/') },
+  },
+});
